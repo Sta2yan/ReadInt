dddaaac86b0eccaf75efb70446990481937959ed
Sta2yan <s2_stas@mail.ru>
2022-03-02 19:24:44 +0300

    Добавьте файлы проекта.

---
+++ ReadInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string userInput = "";
+
+            do
+            {
+                Console.WriteLine("Введите число:");
+                userInput = Console.ReadLine();
+            } while (int.TryParse(userInput, out number) == false);
+
+            Console.WriteLine($"Ваше число: {number}");
+        }
+    }
+}
